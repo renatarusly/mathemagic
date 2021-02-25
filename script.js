ba8659477cc533f8bc4ac14756b1c967c94373ef
renatarusly <renata.rusly@gmail.com>
2021-02-25 14:37:24 +0700

    complete implementation for mathemagic website

---
+++ script.js
@@ -0,0 +1,173 @@
+function a()
+{
+var a=document.getElementById("test1");
+if((a.value=="1520cm3")||(a.value=="1520"))
+{
+document.getElementById('err1').innerHTML=  'correct';
+}
+else
+{
+document.getElementById('err1').innerHTML= 'wrong ';
+}
+{
+document.getElementById('penyelesaian1').innerHTML= "Penyelesaian : <br> diketahui: <br> p = 30 <br> l = 14 <br> t = 10 <br> Luas Permukaan Balok = 2 (pl+pt+lt) <br> = 2 x ( 30×14 ) + ( 20×10 ) + ( 14×10 ) <br> = 2 x ( 420 + 200 + 140 ) <br> = 2 x 760 <br> = 1520 cm2 <br> jadi luas permukaan balok tersebut ialah 1,520 cm2" ;
+}
+}
+
+function b()
+{
+var b=document.getElementById("test2");
+if((b.value=="600")||(b.value=="600cm2"))
+{
+document.getElementById('err2').innerHTML='correct';
+}
+else
+{
+document.getElementById('err2').innerHTML= 'wrong';
+}
+{
+document.getElementById('penyelesaian2').innerHTML= "Penyelesaian : <br> Diketahui : s = 10 cm <br> ditanya : L = …? <br> L = 6 x s2 <br> L = 6 x 10 x 10 <br> L = 600 cm2 <br> Jadi, luasnya permukaan kubus itu adalah = 600 cm2" ; 
+}
+}
+
+function c()
+{
+var c=document.getElementById("test3");
+if((c.value=="212cm")||(c.value=="212"))
+{
+document.getElementById('err3').innerHTML=  'correct';
+}
+else
+{
+document.getElementById('err3').innerHTML= 'wrong';
+}
+{
+document.getElementById('penyelesaian3').innerHTML= 'Penyelesaian: <br> diketahui: p= 30 cm <br> l= 15 cm <br> t= 8 cm <br> Keliling Balok = 4 ( p + l + t ) <br> = 4 (30 + 15 + 8) <br> = 4 x 53 <br> = 212 cm <br> Jadi keliling balok tersebut adalah 212 cm' ;
+}
+}
+
+function d()
+{
+var d=document.getElementById("test4");
+if((d.value=="120cm")||(d.value=="120"))
+{
+document.getElementById('err4').innerHTML='correct';
+}
+else
+{
+document.getElementById('err4').innerHTML= 'wrong';
+}
+{
+document.getElementById('penyelesaian4').innerHTML= '‘Penyelesaian: <br> diketahui: s= 10cm <br> Keliling Kubus = 12 x s <br> = 12 x 10 <br> = 120 cm <br> Jadi keliling kubus tersebut adalah 120 cm' ;
+}
+}
+
+function e()
+{
+var e=document.getElementById("test5");
+if((e.value=="52 cm")||(e.value=="52"))
+{
+document.getElementById('err5').innerHTML='correct';
+}
+else
+{
+document.getElementById('err5').innerHTML= 'wrong';
+}
+{
+document.getElementById('penyelesaian5').innerHTML= 'Penyelesaian: <br> diketahui: s= 13cm <br> k = 156 <br> Keliling Alas= 4 x s <br> = 4 x 13 <br> = 52 cm <br> Jadi keliling alas kubus tersebut adalah 52 cm' ; 
+}
+}
+function f()
+{
+var f=document.getElementById("test6");
+if((f.value=="11 cm")||(f.value=="11"))
+{
+document.getElementById('err6').innerHTML='correct';
+}
+else
+{
+document.getElementById('err6').innerHTML= 'wrong';
+}
+{
+document.getElementById('penyelesaian6').innerHTML= 'Penyelesaian: <br> diketahui: p = 6 cm <br> t = 9 cm <br> l = 2 cm <br> hasil: √p²+l²+t² <br> = √6² + 2²+9² <br> = √36+4+81 <br> = √121 <br> = 11 cm <br> Jadi panjang diagonal ruang balok tersebut adalah 11 cm.';
+}
+}
+function g()
+{
+var g=document.getElementById("test7");
+if((g.value=="12√3 cm")||(g.value=="12 akar 3"))
+{
+document.getElementById('err7').innerHTML='correct';
+}
+else
+{
+document.getElementById('err7').innerHTML= 'wrong';
+}
+{
+document.getElementById('penyelesaian7').innerHTML= 'Penyelesaian: <br> Panjang diagonal ruang= s√3 <br> 12√3 cm <br> Jadi panjang diagonal ruang kubus tersebut adalah 12√3 cm.';
+}
+}
+
+function h()
+{
+var h=document.getElementById("test8");
+if((h.value=="13cm")||(h.value=="13"))
+{
+document.getElementById('err8').innerHTML='correct';
+}
+else
+{
+document.getElementById('err8').innerHTML= 'wrong';
+}
+{
+document.getElementById('penyelesaian8').innerHTML= 'Penyelesaian: <br> diketahui: AB = 12cm <br> AE = BF = 5cm <br> AF = √(AB2 + BF2) <br> AF = √(122 + 52) <br> AF = √(144+25) <br> AF = √169 <br> AF = 13cm <br> Jadi panjang AF adalah 13 cm.';
+}
+}
+
+function i()
+{
+var i=document.getElementById("test9");
+if((i.value=="8√2cm")||(i.value=="8 akar 2"))
+{
+document.getElementById('err9').innerHTML='correct';
+}
+else
+{
+document.getElementById('err9').innerHTML= 'wrong';
+}
+{
+document.getElementById('penyelesaian9').innerHTML= 'Penyelesaian: <br> diagonal sisi= x√2 <br> diagonal sisi= 8√2 cm. <br> Jadi panjang diagonal bidangnya adalah 8√2 cm.';
+}
+}
+
+function j()
+{
+var j=document.getElementById("test10");
+if((j.value=="120000cm3")||(j.value=="120.000"))
+{
+document.getElementById('err10').innerHTML='correct';
+}
+else
+{
+document.getElementById('err10').innerHTML= 'wrong';
+}
+{
+document.getElementById('penyelesaian10').innerHTML= 'Penyelesaian:<br> p = 60 <br> l = 40 <br> t = 50 <br> V = p x l x t <br> V = 60 x 40 x 50 <br> V = 120.000 cm3. <br> Jadi, volume akuarium tersebut adalah 120.000 cm3.';
+}
+}
+
+function k()
+{
+var k=document.getElementById("test11");
+if((k.value=="125cm3")||(k.value=="125"))
+{
+document.getElementById('err11').innerHTML='correct';
+}
+else
+{
+document.getElementById('err11').innerHTML= 'wrong';
+}
+{
+document.getElementById('penyelesaian11').innerHTML= 'Penyelesaian:<br>V = s3 <br>V = (5 cm)3<br?V = 125 cm3<br>Jadi, volume kubus tersebut adalah  125 cm3';
+}
+}
